b02b31b67fa747e1f428df573efd14f9e174d8e8
JoshuaPhagu <acidcentaur@gmail.com>
2025-01-30 19:15:25 -0500

    minor changes

--- FrontEnd/FrontEnd/obj/Release/net8.0/FrontEnd.AssemblyInfo.cs
+++ FrontEnd/FrontEnd/obj/Release/net8.0/FrontEnd.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("FrontEnd")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c301fa7b90358f1f98b5efd29056308856f3a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("FrontEnd")]
